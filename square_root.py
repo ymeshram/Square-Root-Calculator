f84ab32476d922782ffd9a8183488f7686844f77
ymeshram <165543320+ymeshram@users.noreply.github.com>
2024-11-11 23:47:22 +0530

    Update square_root.py

--- square_root.py
+++ square_root.py
@@ -2,4 +2,4 @@ import math
 def square_root(num):
     if num < 0:
         raise ValueError("Cannot calculate square root of a negative number")
-    return math.sqrt(value)
+    return math.sqrt(num)
